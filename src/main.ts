dbd918ca779b9e6d8160acc86a93e9883d3c198d
Tele Falola <oluwateledemi@gmail.com>
2024-09-30 00:35:08 +0100

    chore: set up swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger('Bootstrap');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
+  );
+  const configService = app.get(ConfigService);
+  app.disable('x-powered-by', 'X-Powered-By');
+  const config = new DocumentBuilder()
+    .setTitle('Starwar Backend')
+    .setDescription('Starwar backend operations API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  logger.log(
+    `Starting application on http://localhost:${configService.get('PORT')}`,
+  );
+  await app.listen(configService.get('PORT') as number);
 }
 bootstrap();
--- src/modules/people/controllers/people.controller.ts
+++ src/modules/people/controllers/people.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Query } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { PeopleService } from '../services/people.service';
 import { QueryDto } from 'src/common/dtos/query.dto';
 import { PaginatedPeopleResponse } from '../dtos/people';
 
 @Controller('people')
+@ApiTags('People')
 export class PeopleController {
   constructor(private readonly peopleService: PeopleService) {}
-
+  @ApiOkResponse({
+    type: PaginatedPeopleResponse,
+  })
+  @ApiQuery({
+    name: 'page',
+    description: 'Page number for paginated results, defaults to 1.',
+    required: false,
+    type: 'number',
+  })
+  @ApiQuery({
+    name: 'search',
+    description: 'Search term to filter results. Optional.',
+    required: false,
+    type: 'string',
+  })
+  @Get('/')
   async getPeople(@Query() query: QueryDto) {
     const { page, search } = query;
     const { count, prev, next, results } = await this.peopleService.getPeople(
