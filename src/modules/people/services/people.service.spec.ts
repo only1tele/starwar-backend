e971d74269ba2d8e945e33f1598143ce6ddd9369
Tele Falola <oluwateledemi@gmail.com>
2024-09-30 02:41:03 +0100

    test: add unit tests for people and planets module

--- src/modules/people/services/people.service.spec.ts
+++ src/modules/people/services/people.service.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PeopleService } from './people.service';
+import { StarWarService } from 'src/providers/starwar/starwar.service';
+import { RedisCacheService } from 'src/common/redis-cache/redis-cache.service';
 
 describe('PeopleService', () => {
   let service: PeopleService;
+  let starWarService: StarWarService;
+  let redisCacheService: RedisCacheService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PeopleService],
+      providers: [
+        PeopleService,
+        {
+          provide: StarWarService,
+          useValue: {
+            getPeopleByID: jest.fn(),
+            getPeople: jest.fn(),
+            getByUrl: jest.fn(),
+          },
+        },
+        {
+          provide: RedisCacheService,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PeopleService>(PeopleService);
+    starWarService = module.get<StarWarService>(StarWarService);
+    redisCacheService = module.get<RedisCacheService>(RedisCacheService);
   });
 
   it('should be defined', () => {
--- src/modules/planets/services/planets.service.spec.ts
+++ src/modules/planets/services/planets.service.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PlanetsService } from './planets.service';
+import { StarWarService } from 'src/providers/starwar/starwar.service';
+import { RedisCacheService } from 'src/common/redis-cache/redis-cache.service';
 
 describe('PlanetsService', () => {
   let service: PlanetsService;
+  let starWarService: StarWarService;
+  let redisCacheService: RedisCacheService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PlanetsService],
+      providers: [
+        PlanetsService,
+        {
+          provide: StarWarService,
+          useValue: {
+            getPlanetByID: jest.fn(),
+            getPlanets: jest.fn(),
+            getByUrl: jest.fn(),
+          },
+        },
+        {
+          provide: RedisCacheService,
+          useValue: {
+            get: jest.fn(),
+            set: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PlanetsService>(PlanetsService);
+    starWarService = module.get<StarWarService>(StarWarService);
+    redisCacheService = module.get<RedisCacheService>(RedisCacheService);
   });
 
   it('should be defined', () => {
