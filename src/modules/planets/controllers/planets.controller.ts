e6e24ba35055dc4f23004ecc4c4f84602a407d19
Tele Falola <oluwateledemi@gmail.com>
2024-09-30 01:18:21 +0100

    feat: implement GET /planets endpoint

--- src/modules/planets/controllers/planets.controller.ts
+++ src/modules/planets/controllers/planets.controller.ts
@@ -1,9 +1,42 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOkResponse, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { PlanetsService } from '../services/planets.service';
+import { QueryDto } from 'src/common/dtos/query.dto';
+import { PaginatedPlanetResponse } from '../dtos/planets';
 
 @Controller('planets')
 @ApiTags('Planets')
 export class PlanetsController {
   constructor(private readonly planetsService: PlanetsService) {}
+
+  @ApiOkResponse({
+    type: PaginatedPlanetResponse,
+  })
+  @ApiQuery({
+    name: 'page',
+    description: 'Page number for paginated results, defaults to 1.',
+    required: false,
+    type: 'number',
+  })
+  @ApiQuery({
+    name: 'search',
+    description: 'Search term to filter results. Optional.',
+    required: false,
+    type: 'string',
+  })
+  @Get('/')
+  async getPlanets(@Query() query: QueryDto) {
+    const { page, search } = query;
+    const { count, prev, next, results } = await this.planetsService.getPlanets(
+      page,
+      search,
+    );
+
+    return {
+      count,
+      prev,
+      next,
+      results,
+    } as PaginatedPlanetResponse;
+  }
 }
--- src/modules/planets/services/planets.service.ts
+++ src/modules/planets/services/planets.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { StarWarService } from 'src/providers/starwar/starwar.service';
 import { RedisCacheService } from 'src/common/redis-cache/redis-cache.service';
+import { extractIdFromUrl, extractPageNumber } from 'src/utils/pagination.util';
+import { PaginatedPlanetResponse, PlanetBase } from '../dtos/planets';
+import { StarwarPlanet } from 'src/providers/starwar/starwar.type';
 
 @Injectable()
 export class PlanetsService {
@@ -8,4 +11,41 @@ export class PlanetsService {
     private readonly starWarService: StarWarService,
     private readonly redisCacheService: RedisCacheService,
   ) {}
+
+  async getPlanets(
+    page: number,
+    search: string,
+  ): Promise<PaginatedPlanetResponse> {
+    const cacheKey = `planets_page_${page}`;
+
+    const cachedData = await this.redisCacheService.get(cacheKey);
+    if (cachedData) return cachedData;
+
+    const data = await this.starWarService.getPlanets(page, search);
+    const paginatedResponse: PaginatedPlanetResponse = {
+      count: data.count,
+      next: extractPageNumber(data.next),
+      prev: extractPageNumber(data.previous),
+      results: data.results.map(this.mapToPlanetBase),
+    };
+
+    await this.redisCacheService.set(cacheKey, paginatedResponse, 300);
+
+    return paginatedResponse;
+  }
+
+  private mapToPlanetBase(planet: StarwarPlanet) {
+    return {
+      id: extractIdFromUrl(planet.url),
+      name: planet.name,
+      rotation_period: planet.rotation_period,
+      orbital_period: planet.orbital_period,
+      diameter: planet.diameter,
+      climate: planet.climate,
+      gravity: planet.gravity,
+      terrain: planet.terrain,
+      surface_water: planet.surface_water,
+      population: planet.population,
+    } as PlanetBase;
+  }
 }
